bf37713219c94996fb5e1a0e5545cdd3694d0369
+ <omuhindaaaron@gmail.com>
2024-07-03 12:21:45 +0300

    Building a GET API Lab.

--- server/app.py
+++ server/app.py
@@ -20,19 +20,23 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    return make_response(jsonify([bakery.to_dict() for bakery in bakeries]), 200)
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
-
+    bakeries = Bakery.query.get(id)
+    return make_response(jsonify(bakeries.to_dict()), 200)
+    
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    return make_response(jsonify([baked_good.to_dict() for baked_good in baked_goods]), 200)
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    return make_response(jsonify(baked_goods.to_dict()), 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
